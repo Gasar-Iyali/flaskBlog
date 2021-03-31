a688bbfbde94104e1c08921006451ad6392dfef1
Zack Amata <gasariyali09@gmail.com>
2021-03-31 09:54:34 +0100

    added index, about and zack html pages

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return "Hello World. Welcome to Zack Amata's Flask Blog!"
+    return "<h1>Home Page of Zack Amata's Flask Blog</h1>Hello World. Welcome to Zack Amata's Flask Blog!"
 
 @app.route('/zack')
 def zack():
-    return "Second page of Zack Amata's Flask Blog!"
+    return "Second page of Zack Amata's Flask Blog!<h1> Hi there Zack Amata</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>About Page! This page tells you all about Zack Amata's Flask Blog!</h1>"
 
 
     if __name__ == '__main__':
